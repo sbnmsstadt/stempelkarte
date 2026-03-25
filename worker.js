32946ae02acde25ca9b2b4f7e2a835e2c80b80a6
sbnmsstadt <sb.nmsstadt@outlook.com>
2026-03-25 16:47:29 +0100

    Add files via upload

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ export default {
 
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
     };
 
